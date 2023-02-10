836174f7933eeb7d96a452dbdc89541dc327c8e3
ginnoro2 <84360244+ginnoro2@users.noreply.github.com>
2023-02-10 09:00:11 +0545

    Add files via upload
    first commit

---
+++ coursework1/app.py
@@ -0,0 +1,79 @@
+from flask import *
+from twilio.rest import Client
+import random
+import subprocess
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'otp'#secret key session is written over cookies
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get_db', methods=['GET'])
+def get_db():
+    conn = sqlite3.connect('student.db')
+    cursor = conn.cursor()
+    cursor =  conn.execute('SELECT * FROM ADMIN')
+    results = cursor.fetchall()
+    print("ID\tUSERNAME\tPASSWORD")
+    for row in cursor:
+        print("{}\t{}\t{}".format(row[0], row[1], row[2]))
+
+    conn.close()
+    return results
+
+#@app.route('/')
+def result():
+    get = get_db()
+    print(get)
+    
+    return render_template('out.html',usr=get)
+
+@app.route('/getOTP', methods = ['POST'])
+def getOTP():
+    number = request.form['number']
+    val = getOTPApi(number)
+    #if val:    
+    return render_template('login.html')
+
+@app.route('/validateOTP', methods = ['POST'])
+def validateOTP():
+    otp = request.form['otp']
+    if 'response' in session:
+        s= session['response']
+        session.pop('response',None)
+        if s == otp:
+            return redirect(url_for('get_db'))
+            
+            
+        else:
+            return render_template('server.html')
+
+def generateOTP():
+    return random.randrange(100000, 999999)
+
+def getOTPApi(number):
+    account_sid = 'ACc4ac69fcb07e679ef0d306f17c2fe316'
+    auth_token = 'c47bf426991792f2e2cdc15d39b4851c'
+    client = Client(account_sid, auth_token)
+    otp = generateOTP()
+    session['response'] = str(otp)
+    body = 'Sulav is BOKA    : ' + str(otp)
+    message = client.messages.create(
+                              body=body,
+                              from_='+13855264732',
+                              to=number
+                          )
+    if message.sid:
+        True
+    else:
+        False
+
+    print(message.sid)
+def term():
+    os.system('docker build -t testbuntu:latest .')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ coursework1/test.py
@@ -0,0 +1,79 @@
+from tkinter import *
+#import library
+import sqlite3
+import webbrowser
+import os
+import sys
+#open databse
+
+#defining login function
+def login():
+    #getting form data
+    uname=username.get()
+    pwd=password.get()
+    #applying empty validation
+    if uname=='' or pwd=='':
+        message.set("fill the empty field!!!")
+    else:
+      #open database
+      conn = sqlite3.connect('student.db')
+      #select query
+      cursor = conn.execute('SELECT * from ADMIN where USERNAME="%s" and PASSWORD="%s"'%(uname,pwd))
+      #fetch data 
+      if cursor.fetchone():
+       message.set("Login success")
+       dock()
+
+      else:
+       message.set("Wrong username or password!!!")
+       
+       for i in range (0, 4):
+           webbrowser.open("http://localhost:5000")
+           
+           redirect()
+
+       
+       #webbrowser.open("http://localhost:5000")
+       #redirect()
+        
+#defining loginform function
+def redirect():
+    os.system('python3 app.py')
+    
+    
+def dock():
+    os.system('docker run --rm -it -p 5000:80 ubuntu:latest bin/bash')
+
+def Loginform():
+    global login_screen
+    login_screen = Tk()
+    #Setting title of screen
+    login_screen.title("MANGO-SERVER")
+    #setting height and width of screen
+    login_screen.geometry("350x250")
+    login_screen["bg"]="#1C2833"
+    #declaring variable
+    global  message;
+    global username
+    global password
+    username = StringVar()
+    password = StringVar()
+    message=StringVar()
+    #Creating layout of login form
+    Label(login_screen,width="300", text="Login From", bg="#0E6655",fg="white",font=("Arial",12,"bold")).pack()
+    #Username Label
+    Label(login_screen, text="Username * ",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=20,y=40)
+    #Username textbox
+    Entry(login_screen, textvariable=username,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=120,y=42)
+    #Password Label
+    Label(login_screen, text="Password * ",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=20,y=80)
+    #Password textbox
+    Entry(login_screen, textvariable=password ,show="*",bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=120,y=82)
+    #Label for displaying login status[success/failed]
+    Label(login_screen, text="",textvariable=message,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=95,y=120)
+    #Login button
+    Button(login_screen, text="Login", width=10, height=1, command=lambda: [login()], bg="#0E6655",fg="white",font=("Arial",12,"bold")).place(x=125,y=170)
+    login_screen.mainloop()
+#calling function Loginform
+Loginform()
+
