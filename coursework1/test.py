504a66a126fd3a27d823cbacc15c215d9428628f
ginnoro2 <84360244+ginnoro2@users.noreply.github.com>
2023-02-22 23:15:29 +0545

    Update test.py
    added register form and database.

--- coursework1/test.py
+++ coursework1/test.py
@@ -4,13 +4,15 @@ import sqlite3
 import webbrowser
 import os
 import sys
-#open databse
+#Open Databse
 
 #defining login function
 def login():
     #getting form data
     uname=username.get()
     pwd=password.get()
+
+    print("you have logged in to server")
     #applying empty validation
     if uname=='' or pwd=='':
         message.set("fill the empty field!!!")
@@ -39,8 +41,36 @@ def login():
 #defining loginform function
 def redirect():
     os.system('python3 app.py')
-    
-    
+  
+
+def Register():
+    uname=username.get()
+    pwd=password.get()
+
+    print(uname)
+    print(pwd)
+    if uname=='' or pwd=='':
+        message.set("fill the empty field!!!")
+
+       #print("connectede to database")
+    else:
+        conn = sqlite3.connect('student.db')
+        print("Connected to Database")
+        
+        conn.execute("""INSERT INTO ADMIN(USERNAME, PASSWORD) VALUES (?, ?)""",(uname, pwd))
+
+        conn.commit()
+
+        value = conn.execute('SELECT * FROM ADMIN WHERE USERNAME="%s"'%(uname))
+        print(value.fetchone())
+        message.set("Account Created")
+
+        conn.close()
+
+    login_screen.destroy()
+
+    Loginform()
+            
 def dock():
     os.system('docker run --rm -it -p 5000:80 ubuntu:latest bin/bash')
 
@@ -48,7 +78,7 @@ def Loginform():
     global login_screen
     login_screen = Tk()
     #Setting title of screen
-    login_screen.title("MANGO-SERVER")
+    login_screen.title("SERVER LOGIN")
     #setting height and width of screen
     login_screen.geometry("350x250")
     login_screen["bg"]="#1C2833"
@@ -72,8 +102,10 @@ def Loginform():
     #Label for displaying login status[success/failed]
     Label(login_screen, text="",textvariable=message,bg="#1C2833",fg="white",font=("Arial",12,"bold")).place(x=95,y=120)
     #Login button
-    Button(login_screen, text="Login", width=10, height=1, command=lambda: [login()], bg="#0E6655",fg="white",font=("Arial",12,"bold")).place(x=125,y=170)
+    Button(login_screen, text="Login", width=10, height=1, command=lambda: [login()], bg="#0E6655",font=("Arial",12,"bold")).place(x=125,y=170)
+    #login_screen.mainloop()
+
+    Button(login_screen, text="Register", width=10, height=1, command=lambda: [Register()], bg="#0E6655",font=("Arial",12,"bold")).place(x=125,y=210)
     login_screen.mainloop()
 #calling function Loginform
 Loginform()
-
