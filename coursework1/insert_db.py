c6dc04d41a248a787fc89edd9f933e8feb9a8ce3
ginnoro2 <84360244+ginnoro2@users.noreply.github.com>
2023-02-25 23:35:24 +0545

    Update insert_db.py

--- coursework1/insert_db.py
+++ coursework1/insert_db.py
@@ -2,9 +2,9 @@ import  sqlite3
 conn=sqlite3.connect("student.db")
 print("Database Opened successfully")
 
-conn.execute("INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('sudip', 'aila')");
+conn.execute("INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('admin', 'admin')");
 
-conn.execute("INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('danger', 'zone')");
+conn.execute("INSERT INTO ADMIN(USERNAME,PASSWORD) VALUES ('root', 'root')");
 
 conn.commit()
 print ("Records inserted successfully")
